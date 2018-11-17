473873f89111dc53a6deb939a315ddab7b8f31fa
J.Lee <joverlee521@gmail.com>
2018-11-17 14:13:13 -0800

    added comments

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
+// Dependencies
 var express = require("express");
+var path = require("path");
+var exphbs = require("express-handlebars");
+
+// Setting up the express app
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Setting up data parsing for app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var path = require("path");
+// Static directory
 app.use(express.static(path.join(__dirname, "/public")));
 
-var exphbs = require("express-handlebars");
+// Setting up handlbars for express app
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Routes
 var router = require("./controllers/burgers_controller");
 app.use(router);
 
+// Starting express app
 app.listen(PORT, function(){
     console.log("Server is listening on: http://localhost:" + PORT);
 });
