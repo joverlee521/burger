4d280c843e0f83f2afc4081fcdd4360ba497383d
J.Lee <joverlee521@gmail.com>
2018-11-17 19:55:05 -0800

    added comments

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,27 @@
+// Importing ORM object
 var orm = require("../config/orm");
 
+// Burger model object that uses ORM methods
 var burger = {
+    // Method to get all burger data
     all: function(cb){
         orm.selectAll("burgers", "updated_at", "ASC", function(res){
             cb(res);
         });
     },
+    // Method to add burger
     insert: function(cols, vals, cb){
         orm.insertOne("burgers", cols, vals, function(res){
             cb(res);
         });
     },
+    // Method to update burger
     update: function(colValObj, id, cb){
         orm.updateOne("burgers", colValObj, id, function(res){
             cb(res);
         });
     },
+    // Method to delete burger
     delete: function(id, cb){
         orm.delete("burgers", id, function(res){
             cb(res);
