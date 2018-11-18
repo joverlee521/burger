34d7a82f0963d164655c358eea351822d34de52c
J.Lee <joverlee521@gmail.com>
2018-11-17 19:46:11 -0800

    added comments

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 // Set up MySQL connection
 var mysql = require("mysql");
-
 var connection;
 
+// Connect to JawsDB if availabe, else connect to localhost
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
@@ -14,7 +14,6 @@ if(process.env.JAWSDB_URL){
         database: "burgers_db"
     });
 }
-
 
 // Make connection to MySQL database
 connection.connect(function(err){
--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,7 @@
+// Import MySQL connection
 var connection = require("./connection");
 
+// Function for generating question marks for column values 
 function addQuestionMarks(length){
     var array = [];
     for(var i = 0; i < length; i++){
@@ -8,6 +10,7 @@ function addQuestionMarks(length){
     return array.toString();
 }
 
+// Function for converting an object to a string in MySQL query
 function objToSql(ob){
     var array = [];
     for(var key in ob){
@@ -16,13 +19,16 @@ function objToSql(ob){
             if(typeof value === "string" && value.indexOf(" ") >= 0){
                 value = "'" + value + "'";
             }
+            // Generates a string such as burger_name='Juicy Lucy Burger'
             array.push(key + "=" + value);
         }
     }
     return array.toString();
 }
 
+// ORM object
 var orm = {
+    // Method to select all from table
     selectAll: function(table, orderCol, order, cb){
         var queryString = "SELECT * FROM " + table + " ORDER BY " + orderCol + " " + order + ";";
         connection.query(queryString, function(err, result){
@@ -30,6 +36,7 @@ var orm = {
             cb(result);
         });
     },
+    // Method to insert one row into table
     insertOne: function(table, cols, vals, cb){
         var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") VALUES (" + addQuestionMarks(vals.length) +  ");";
         connection.query(queryString,vals, function(err, result){
@@ -37,6 +44,7 @@ var orm = {
             cb(result);
         });
     },
+    // Method to update one row in table
     updateOne: function(table, colValObj, id, cb){
         var queryString = "UPDATE " + table + " SET " + objToSql(colValObj) + " WHERE " + id;
         connection.query(queryString, function(err, result){
@@ -44,6 +52,7 @@ var orm = {
             cb(result);
         });
     },
+    // Method to delete one row in table
     delete: function(table, id, cb){
         var queryString = "DELETE FROM " + table + " WHERE " + id;
         connection.query(queryString, function(err, result){
